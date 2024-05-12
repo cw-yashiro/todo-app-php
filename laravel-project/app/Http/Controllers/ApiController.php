a6526f1fc7e9687b085a2d69e4a0eb6ecbc41850
cw-ryo.yashiro <>
2024-05-12 17:47:57 +0900

    feat: controllerのメソッドを定義

--- laravel-project/app/Http/Controllers/ApiController.php
+++ laravel-project/app/Http/Controllers/ApiController.php
@@ -6,5 +6,19 @@ use Illuminate\Http\Request;
 
 class ApiController extends Controller
 {
-    //
+    public function getAllTasks() {
+        // logic to get all task goes here
+    }
+    
+    public function createTask(Request $request) {
+        // logic to create a task record goes here
+    }
+    
+    public function updateTask(Request $request, $id) {
+        // logic to update a task record goes here
+    }
+    
+    public function deleteTask ($id) {
+        // logic to delete a task record goes here
+    }
 }
