58d37ad85e242f9894523a351f6bc7582b2ca838
cw-ryo.yashiro <>
2024-05-14 00:10:36 +0900

    feat: テストが動くように設定ファイルなどを追加

--- frontend/vitest.config.ts
+++ frontend/vitest.config.ts
@@ -15,6 +15,8 @@ export default defineConfig({
     open: true,
   },
   test: {
+    globals: true,
     environment: "jsdom",
+    setupFiles: "./src/tests/setup.ts",
   },
 } as VitestConfigExport);
