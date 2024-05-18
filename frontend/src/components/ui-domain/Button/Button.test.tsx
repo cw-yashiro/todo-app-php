a1a3c5ef2ff805ccf2a6a24fb2a4785ea1b1d47a
cw-ryo.yashiro <>
2024-05-19 01:58:24 +0900

    refactor: ButtonコンポーネントをforwardRefで書き換え

--- frontend/src/components/ui-domain/Button/Button.test.tsx
+++ frontend/src/components/ui-domain/Button/Button.test.tsx
@@ -1,10 +1,10 @@
-import { describe, it, expect } from 'vitest'
-import { screen, render } from '@testing-library/react'
-import { Button } from './Button'
+import { describe, it, expect } from "vitest"
+import { screen, render } from "@testing-library/react"
+import { Button } from "./Button"
 
-describe('ui-domain/Button', () => {
-	it('buttonロールを持っていること', () => {
-		render(<Button variant="primary" label="ボタン" onClick={() => { }} />)
-		expect(screen.getByRole('button', { name: "ボタン" })).toBeInTheDocument()
-	})
+describe("ui-domain/Button", () => {
+  it("buttonロールを持っていること", () => {
+    render(<Button label="ボタン" />)
+    expect(screen.getByRole("button", { name: "ボタン" })).toBeInTheDocument()
+  })
 })
