97319d300c4056c36d038919a5b1dbfe755158ac
cw-ryo.yashiro <>
2024-05-21 23:45:23 +0900

    refactor: buttonのスタイルを調整

--- frontend/src/components/ui-domain/Button/Button.tsx
+++ frontend/src/components/ui-domain/Button/Button.tsx
@@ -1,13 +1,17 @@
 import { forwardRef } from "react"
+import style from "./style.module.css"
 
 type Props = {
   label: string
+  variant?: "primary" | "secondary" | "danger"
 } & React.ComponentPropsWithRef<"button">
 
-export const Button = forwardRef(({ label, ...props }: Props) => {
-  return (
-    <>
-      <button {...props}>{label}</button>
-    </>
-  )
-})
+export const Button = forwardRef(
+  ({ label, variant = "primary", ...props }: Props) => {
+    return (
+      <button className={style.module} {...props} data-variant={variant}>
+        {label}
+      </button>
+    )
+  }
+)
