91b13acbf5971902093a343bbc953c6163486435
cw-ryo.yashiro <>
2024-06-03 22:50:31 +0900

    feat: buttonを非活性化するテストを追加

--- frontend/src/components/ui-domain/Button/Button.test.tsx
+++ frontend/src/components/ui-domain/Button/Button.test.tsx
@@ -7,4 +7,9 @@ describe("ui-domain/Button", () => {
     render(<Button label="ボタン" />)
     expect(screen.getByRole("button", { name: "ボタン" })).toBeInTheDocument()
   })
+
+  it("buttonロールを持っていて、非活性化していること", () => {
+    render(<Button label="ボタン" disabled />)
+    expect(screen.getByRole("button", { name: "ボタン" })).toBeDisabled()
+  })
 })
