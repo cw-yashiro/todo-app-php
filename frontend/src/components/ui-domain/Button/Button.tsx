05c4045c04f3cd3ee654c89ba38d97e6e8705ad1
cw-ryo.yashiro <>
2024-06-03 23:17:44 +0900

    refactor: buttonのlabelを..props経由で渡すように変更

--- frontend/src/components/ui-domain/Button/Button.tsx
+++ frontend/src/components/ui-domain/Button/Button.tsx
@@ -2,16 +2,9 @@ import { forwardRef } from "react"
 import style from "./style.module.css"
 
 type Props = {
-  label: string
   variant?: "primary" | "secondary" | "danger"
 } & React.ComponentPropsWithRef<"button">
 
-export const Button = forwardRef(
-  ({ label, variant = "primary", ...props }: Props) => {
-    return (
-      <button className={style.module} {...props} data-variant={variant}>
-        {label}
-      </button>
-    )
-  }
-)
+export const Button = forwardRef(({ variant = "primary", ...props }: Props) => {
+  return <button className={style.module} {...props} data-variant={variant} />
+})
